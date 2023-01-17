ad575cbc8ee076029646ea4a2b0fe1571124f698
fhahroul Rozi <fahrulrozi99321@gmail.com>
2023-01-17 12:59:29 +0700

    Update: deleted data relasi

--- resources/views/master_data/cancellation_policy/edit.blade.php
+++ resources/views/master_data/cancellation_policy/edit.blade.php
@@ -7,45 +7,27 @@
         <div class="row">
             <div class="panel panel-default">
                 <div class="panel-body shadow">
-                    {{-- <form id="delete_room" onsubmit="return confirm('Are you sure ?')" method="POST"
-                        action="{{ route('cancellation_policy.delete') }}" enctype="multipart/form-data" autocomplete="off">
+                    <form id="delete_room" onsubmit="return confirm('Are you sure ?')" method="POST"
+                        action="{{ route('cancellation_policy.delete', $cancellationpolicies->id) }}" enctype="multipart/form-data" autocomplete="off">
                         <input type="hidden" name="id" id="cancellation-id" value="{{ $cancellationpolicies->id }}">
                         {{ csrf_field() }}
-                    </form> --}}
+                    </form>
                     <form method="POST" action="{{ route('cancellation_policy.update', $cancellationpolicies->id)}}" enctype="multipart/form-data" autocomplete="off">
                         @csrf
-                        {{-- @method('PUT') --}}
                         <div class="row">
                             <div class="col-lg-12 col-md-12">
                                     <label>Cancellation Name</label>
                                     <input type="text" class="form-control" name="name" id="name" placeholder="Masukkan Cancellation" value="{{ $cancellationpolicies->name }}">
-
-                                    <!-- error message untuk title -->
-                                    {{-- @error('name')
-                                        <div class="alert alert-danger mt-2">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror --}}
                                 <br>
                             </div>
                             <div class="col-lg-12 col-md-12">
                                 <label for="description">Description</label><br>
-                                {{-- <input type="text" class="form-control" name="description" id="description" placeholder="Masukkan Cancellation" value="{{ $cancellationpolicies->description }}"> --}}
-
                                 <textarea type="text" class="form-control" name="description" id="description" placeholder="Description" style="padding: 2% 0% 20% 2%;">{{ $cancellationpolicies->description }}</textarea><br><br>
-                                {{-- <textarea type="text" class="form-control" id="description" name="description"
-                                    placeholder="New Description"></textarea> --}}
-                                    {{-- <label for="description">Description</label>
-                                    <input type="text" class="form-control" id="description" name="description"
-                                        value="{{old('description', $description)}}" placeholder="Description"> --}}
-                          {{--           @error('description')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror --}}
                                 <br>
                             </div>
                         </div>
                             <div class="pull-right">
-                                <button type="submit" form="delete_room" class="btn btn-delete btn-padding delete" data-id="{{ $cancellationpolicies->id }}" data-name="{{ $cancellationpolicies->name }}">
+                                <button type="submit" form="delete_room" class="btn btn-delete btn-padding">
                                     Delete
                                 </button>
                                 <a class="btn btn-white btn-padding" href="{{ route('cancellation_policy.index') }}">
@@ -59,32 +41,4 @@
             </div>
         </div>
     </div>
-
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
-    <script>
-        $('.delete').click(function(){
-            var cancellation_id = $(this).attr('data-id');
-            var cancellation = $(this).attr('data-name');
-            swal({
-                title: "Are you sure ?",
-                icon: "warning",
-                buttons: true,
-                dangerMode: true,
-                })
-                .then((willDelete) => {
-                if (willDelete) {
-                    window.location = "/delete/"+cancellation_id+""
-                } else {
-                    // swal("cancellation "+cancellation+" gagal di hapus");
-                    swal(
-                            'Oooops!!!',
-                            'Cancellation gagal di hapus :)',
-                            'error'
-                    )
-                }
-                });
-        });
-
-    </script>
 @endsection
--- routes/Routes/master_data/routes.php
+++ routes/Routes/master_data/routes.php
@@ -69,10 +69,10 @@ Route::group(['middleware' => ['web', 'auth']], function () {
         Route::get('/edit/{id}', ['as' => 'cancellation_policy.edit', 'uses' => 'Master\CancellationController@edit']);
         Route::post('/update/{id}', ['as' => 'cancellation_policy.update', 'uses' => 'Master\CancellationController@update']);
         // Delete
+        Route::post('/delete/{id}', ['as' => 'cancellation_policy.delete', 'uses' => 'Master\CancellationController@delete']);
         // AJAX
         Route::get('/setdata/{id}', ['as' => 'cancellation_policy.setdata', 'uses' => 'Master\CancellationController@setdata']);
     });
-    Route::get('/delete/{id}', [CancellationController::class, 'delete'])->name('delete');
 
     // Panel Amenities
     Route::group(['prefix' => 'master_data/amenities'], function() {
